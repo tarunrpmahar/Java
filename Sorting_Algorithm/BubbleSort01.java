45f66ddb73e3f5a20301b4160e3243fdbdfe8475
Tarun Singh Mahar <62583757+tarunrpmahar@users.noreply.github.com>
2021-03-03 18:42:17 +0530

    Add files via upload

---
+++ Sorting_Algorithm/BubbleSort01.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sorting;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Shubham Singh Mahar
+ */
+public class BubbleSort01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        
+        for (int i = 0; i < n-1; i++) {
+            int count=0;
+            for (int j = 0; j < n-i-1; j++) {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    count++;
+                }
+            }
+            if(count==0)
+            {
+                //System.out.println("i is "+i);
+                break;
+            }          
+        }
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
