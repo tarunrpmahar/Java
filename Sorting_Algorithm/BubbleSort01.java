b17740bebdb414bb0231fa84e81f2fad158753e8
Tarun Singh Mahar <62583757+tarunrpmahar@users.noreply.github.com>
2021-03-03 18:43:09 +0530

    Update BubbleSort01.java

--- Sorting_Algorithm/BubbleSort01.java
+++ Sorting_Algorithm/BubbleSort01.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sorting;
-
 import java.util.Scanner;
 
 /**
  *
- * @author Shubham Singh Mahar
+ * @author Tarun Singh Mahar
  */
 public class BubbleSort01 {
     public static void main(String[] args) {
